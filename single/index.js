03aebd800a9f8dd1ef69fc02b6b1c586f2acf850
aimner <aimner510@gmail.com>
2023-04-06 21:54:45 +0300

    feat: added pets data

--- single/index.js
+++ single/index.js
@@ -1,4 +1,7 @@
+import { PETS } from "../data/data.js";
+
 // SLIDER
+
 const SLIDER = document.querySelector(".slider-list");
 const SLIDES = document.querySelectorAll(".slider-list-item");
 const SLIDE = document.querySelector(".slider-list-item");
@@ -31,22 +34,50 @@ LEFT_BUTTON.addEventListener("click", () => {
 function popup() {
   const POPUP = document.querySelector(".popup");
   const BUTTONS = document.querySelectorAll(".slider-list-item__button");
+  const POPUP_BLOCK = document.querySelector(".popup-container");
 
-  const changePopupClass = () => {
+  const changePopupClass = (id) => {
+    POPUP_BLOCK.innerHTML = `
+    <div class="popup-img">
+    <img src="./assets/imgs/${PETS[id].name}.png" alt="dog" />
+  </div>
+  <div class="popup-description">
+    <h2 class="popup-description__title">${PETS[id].name}</h2>
+    <p class="popup-description__breed">${PETS[id].breed}</p>
+    <p class="popup-description__text">
+    ${PETS[id].text}
+    </p>
+    <ul class="popup-description-list">
+      <li class="popup-description-list-item">
+        <span>Age:</span>
+        <span>2 months</span>
+      </li>
+      <li class="popup-description-list-item">
+        <span> Inoculations:</span>
+        <span> none</span>
+      </li>
+      <li class="popup-description-list-item">
+        <span>Diseases:</span>
+        <span> none</span>
+      </li>
+      <li class="popup-description-list-item">
+        <span>Parasites:</span>
+        <span> none</span>
+      </li>
+    </ul>
+  </div>
+    `;
     POPUP.classList.toggle("popup--notActive");
     document.body.classList.toggle("blockScroll");
-    
   };
 
   POPUP.addEventListener("click", (e) => {
-    if (e.target === POPUP) changePopupClass();
+    if (e.target === POPUP) changePopupClass(0);
   });
 
-  BUTTONS.forEach((item) => {
-    item.addEventListener("click", changePopupClass);
+  BUTTONS.forEach((item, id) => {
+    item.addEventListener("click", () => changePopupClass(id));
   });
-
-
 }
 
 popup();
